465e3a643b41f5601a5c39a907ea76c359a4a8d1
swcide <swcide@gmail.com>
2021-07-10 14:42:40 +0900

    '항해시네마firstcommit'

---
+++ src/main/java/com/hanghae/cinema/HangHaeCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangHaeCinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HangHaeCinemaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanghae/cinema/HangHaeCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangHaeCinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
